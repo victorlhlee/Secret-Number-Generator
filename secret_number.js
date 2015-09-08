f945662c7afe1d0ce20eb2556bb1d6f4a099fa4b
Victor <viclee02@gmail.com>
2015-09-07 14:53:15 -1000

    test passes

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
-'use strict';
+//'use strict';
+
+//Get a random number between 0 and 1000000 (inclusive)
 module.exports = function() {
-  
+
+  var secretNumberGenerator = Math.floor(Math.random() * 1000000); 
+  //math.floor() returns the largest integer less than or equal to a given number
+  //math.random() returns a random number from 0 (inclusive) up to but not including 1 (exclusive)
+  return function(){
+    return secretNumberGenerator;
+  }; 
 };
